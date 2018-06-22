eef72eea0c509723f5a9e7606fbef2d86c225442
HavocPigeon <speeas@gmail.com>
2018-06-22 14:50:46 -0700

    updated app.js component

--- instructor-code/src/App.js
+++ instructor-code/src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      picture: '',
+      name: 'Tyler',
+      friends: []
+    };
+  }
+
+  changeName(name) {
+    this.setState({
+      name: name,
+    })
+  }
+  
   render() {
     return (
-      <div>lkjsdlkfjasdklfj</div>
+      <div>
+        Name: <input value={this.state.name} onChange={(event) => this.changeName(event.target.value)} />
+      </div>
     );
   }
 }
 
 export default App;
-
-export const myString = 'something';
--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+     picture: '',
+     name: '',
+     friends: [{
+       name: 'My 404 friend',
+       picture: 'https://http.cat/404'
+     }, {
+       name: 'My chill cat with the munchies',
+       picture: 'https://http.cat/420'
+     }]
+         };
+  }
+  changeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    })
+  }
+  changePicture = (event) => {
+    this.setState({
+      picture: event.target.value,
+    })
+  }
+
   render() {
     return (
-      <div></div>
+      <div>
+        Name: <input value={this.state.name} onChange={(event) =>
+        this.changeName(event)} />
+        Picture: <input value={this.state.picture} onChange={(event) =>
+        this.changePicture(event)} />
+        <div>
+          {this.state.friends.map(friend => {
+            return (
+              <div>
+              <p><img src={friend.picture} alt={friend.name}/>
+              <b>{friend.name}</b></p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     );
   }
 }
